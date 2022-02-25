1054d01665be99d087341f8f19e935aad70186a7
pcserba <69249923+pcserba@users.noreply.github.com>
2022-02-25 14:05:02 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var data={
+  "categories": [
+    {
+      "name": "abstract_shape",
+      "score": 0.453125
+    },
+    {
+      "name": "others_",
+      "score": 0.46875
+    }
+  ],
+  "description": {
+    "tags": [
+      "logo"
+    ],
+    "captions": [
+      {
+        "text": "logo",
+        "confidence": 0.9942492246627808
+      }
+    ]
+  },
+  "objects": [
+    {
+      "rectangle": {
+        "x": 12,
+        "y": 6,
+        "w": 453,
+        "h": 460
+      },
+      "object": "porthole",
+      "confidence": 0.783,
+      "parent": {
+        "object": "window",
+        "confidence": 0.808
+      }
+    }
+  ],
+  "brands": [
+    {
+      "name": "BMW",
+      "confidence": 0.989,
+      "rectangle": {
+        "x": 0,
+        "y": 0,
+        "w": 474,
+        "h": 474
+      }
+    }
+  ],
+  "requestId": "98055e9b-8593-4655-a21e-852223232799",
+  "metadata": {
+    "height": 474,
+    "width": 474,
+    "format": "Jpeg"
+  }
+};
