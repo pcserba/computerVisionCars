b464cdbab70eb9c16317b0bfcee3aeea570f8278
pcserba <69249923+pcserba@users.noreply.github.com>
2022-07-21 13:57:42 +0200

    Add files via upload

--- js/updateUI.js
+++ js/updateUI.js
@@ -1,7 +1,7 @@
 function showData() {
 	//update the text
 	//build the string
-	txt = 'We detected a <B>' + discomap1Result.results[0].Mh + ' </B> <B>'+obj[0].Cn_short+'</B> with <B>99%</B> confidence.<P><B>Registrations in Europe ('+discomap1Result.results[0].year+'):</B> '+discomap1Result.results[0].countVIN+' vehicles<P><B>Average specific CO<sub>2</sub> Emissions</B>: '+discomap1Result.results[0].avgEWLTP+' g/km<P><B>Average engine capacity:</B> '+discomap1Result.results[0].avgEc+'cm<sup>3</sup><P><B>Average engine power:</B> '+discomap1Result.results[0].avgEp+'kwh<P><B>Average mass in running order:</B> '+discomap1Result.results[0].avgM+'kg';
+	txt = 'We detected a <B>' + discomap1Result.results[0].Mh + ' </B> <B>'+discomap1Result.results[0].Cn_short+'</B> with <B>99%</B> confidence.<P><B>Registrations in Europe ('+discomap1Result.results[0].year+'):</B> '+discomap1Result.results[0].countVIN+' vehicles<P><B>Average specific CO<sub>2</sub> Emissions</B>: '+discomap1Result.results[0].avgEWLTP+' g/km<P><B>Average engine capacity:</B> '+discomap1Result.results[0].avgEc+'cm<sup>3</sup><P><B>Average engine power:</B> '+discomap1Result.results[0].avgEp+'kwh<P><B>Average mass in running order:</B> '+discomap1Result.results[0].avgM+'kg';
 document.getElementById('caption TL').innerHTML=txt;
 document.getElementById('caption BR').innerHTML='<a href=# onclick="showYear();">[Registrations by year]</A><a href=# onclick="showMS();">[Registrations by MS]</A><a href=# onclick="showManufacturer();">[Ewltp evolution of the manufacturer]</A><P> Source: <a href=#>EEA</A></P></div>';
 	//transition opacity
