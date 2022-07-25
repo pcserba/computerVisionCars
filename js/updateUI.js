cf5f65b40ef285949d7f5d3c4eb4034c285298c5
pcserba <69249923+pcserba@users.noreply.github.com>
2022-07-25 14:21:29 +0200

    Add files via upload

--- js/updateUI.js
+++ js/updateUI.js
@@ -3,7 +3,7 @@ function showData(res) {
 	//build the string
 	txt = 'We detected a <B>' + res[0].results[0].mh + ' </B> <B>'+res[0].results[0].Cn_short+'</B> with <B>99%</B> confidence.<P><B>Registrations in Europe ('+res[0].results[0].year+'):</B> '+res[0].results[0].countVIN+' vehicles<P><B>Average specific CO<sub>2</sub> Emissions</B>: '+res[0].results[0].avgEWLTP+' g/km<P><B>Average engine capacity:</B> '+res[0].results[0].avgEc+'cm<sup>3</sup><P><B>Average engine power:</B> '+res[0].results[0].avgEp+'kwh<P><B>Average mass in running order:</B> '+res[0].results[0].avgM+'kg';
 document.getElementById('caption TL').innerHTML=txt;
-document.getElementById('caption BR').innerHTML='<a href=# onclick="showYear(res[2]);">[Registrations by year]</A><a href=# onclick="showMS(res[1]);">[Registrations by MS]</A><a href=# onclick="showManufacturer(res[3]);">[Ewltp evolution of the manufacturer]</A><P> Source: <a href=#>EEA</A></P></div>';
+document.getElementById('caption BR').innerHTML='<a href=# onclick="showYear('+res[2]+');">[Registrations by year]</A><a href=# onclick="showMS('+res[1]+');">[Registrations by MS]</A><a href=# onclick="showManufacturer('+res[3]+');">[Ewltp evolution of the manufacturer]</A><P> Source: <a href=#>EEA</A></P></div>';
 	//transition opacity
 	document.getElementById('caption TL').classList.toggle('show');
 	document.getElementById('caption BR').classList.toggle('show');
