63602724d508dee96e67b714f31aad4201d36f28
pcserba <69249923+pcserba@users.noreply.github.com>
2022-07-25 13:04:13 +0200

    Add files via upload

--- js/updateUI.js
+++ js/updateUI.js
@@ -1,4 +1,4 @@
-function showData() {
+function showData(res) {
 	//update the text
 	//build the string
 	txt = 'We detected a <B>' + res[0].results[0].Mh + ' </B> <B>'+res[0].results[0].Cn_short+'</B> with <B>99%</B> confidence.<P><B>Registrations in Europe ('+discomap1Result.res[0].results[0].year+'):</B> '+res[0].results[0].countVIN+' vehicles<P><B>Average specific CO<sub>2</sub> Emissions</B>: '+res[0].results[0].avgEWLTP+' g/km<P><B>Average engine capacity:</B> '+res[0].results[0].avgEc+'cm<sup>3</sup><P><B>Average engine power:</B> '+res[0].results[0].avgEp+'kwh<P><B>Average mass in running order:</B> '+res[0].results[0].avgM+'kg';
