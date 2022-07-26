a00e2dc47bcda4249abc6c9f22d630bc1dfe0396
pcserba <69249923+pcserba@users.noreply.github.com>
2022-07-26 14:52:37 +0200

    Add files via upload

--- js/updateUI.js
+++ js/updateUI.js
@@ -3,9 +3,9 @@ function showData(res) {
 	//build the string
 	txt = 'We detected a <B>' + res[0].results[0].mh + ' </B> <B>'+res[0].results[0].Cn_short+'</B> with <B>'+Math.round(fromCarnet[0].probability*100)+'%</B> confidence.<P><B>Registrations in Europe ('+res[0].results[0].year+'):</B> '+res[0].results[0].countVIN+' vehicles<P><B>Average specific CO<sub>2</sub> Emissions</B>: '+res[0].results[0].avgEWLTP+' g/km<P><B>Average engine capacity:</B> '+res[0].results[0].avgEc+'cm<sup>3</sup><P><B>Average engine power:</B> '+res[0].results[0].avgEp+'kwh<P><B>Average mass in running order:</B> '+res[0].results[0].avgM+'kg';
 document.getElementById('caption TL').innerHTML=txt;
-document.getElementById('caption BR').innerHTML='<a href=#>[Registrations by year]</A></P></div>';
-document.getElementById('caption BR2').innerHTML='<a href=#>[Registrations by country]</A></P></div>';
-document.getElementById('caption BR3').innerHTML='<a href=#>[Registrations for '+res[0].results[0].mh+']</A></P></div>';
+document.getElementById('caption BR').innerHTML='<a href=#>[Registrations by year]</A></P>';
+document.getElementById('caption BR2').innerHTML='<a href=#>[Registrations by country]</A></P>';
+document.getElementById('caption BR3').innerHTML='<a href=#>[Avg Ewltp for '+res[0].results[0].mh+' by year]</A></P>';
 
 document.getElementById('caption BR').addEventListener('click', function(){
    showYear(res[2].results);  });
