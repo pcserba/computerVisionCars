67c88d7c09f4fad269ed33212dcf3c61e7bef010
pcserba <69249923+pcserba@users.noreply.github.com>
2022-07-28 17:23:50 +0200

    Add files via upload

--- js/webcam.js
+++ js/webcam.js
@@ -2,12 +2,15 @@ var webkam = {
   // (A) INITIALIZE
   hVid : null, hSnaps :null, hTake : null, hSave : null,
   init : () => {
+
+
     // (A1) GET HTML ELEMENTS
     webkam.hVid = document.getElementById("kam-live"),
+    webkam.hSnaps = document.getElementById("kam-snaps"),
     webkam.hTake = document.getElementById("kam-take"),
 
     // (A2) GET USER PERMISSION TO ACCESS CAMERA
-    navigator.mediaDevices.getUserMedia({ video: true })
+    navigator.mediaDevices.getUserMedia({ video: true, facingMode: { exact: "environment" }})
     .then((stream) => {
       // "LIVE FEED" WEB CAM TO <VIDEO>
       webkam.hVid.srcObject = stream;
@@ -22,7 +25,7 @@ var webkam = {
   // (B) HELPER - SNAP VIDEO FRAME TO CANVAS
   snap : () => {
     // (B1) CREATE NEW CANVAS
-    let canvas = webkam.hVid.createElement("canvas"),
+    let canvas = document.createElement("canvas"),
         ctx = canvas.getContext("2d"),
         vWidth = webkam.hVid.videoWidth,
         vHeight = webkam.hVid.videoHeight;
@@ -30,19 +33,25 @@ var webkam = {
     // (B2) CAPTURE VIDEO FRAME TO CANVAS
     canvas.width = vWidth;
     canvas.height = vHeight;
+  webkam.hVid.style.display = "none";
     ctx.drawImage(webkam.hVid, 0, 0, vWidth, vHeight);
-	webkam.hVid.pause();
-	webkam.hVid.removeAttribute('src'); // empty source
-	webkam.hVid.load();
-    
-// (B3) DONE
+
+	canvas.toBlob((blob) => {
+  	let file = new File([blob], "demo.png", { type: "image/png" });
+  	var data = new FormData();
+  	data.append("up", file);
+  	fetch("https://api.carnet.ai/v2/mmg/detect?box_offset=0&box_min_width=180&box_min_height=180&box_min_ratio=1&box_max_ratio=3.15&box_select=center&region=DEF&api-key=91761936-0b93-4f6e-919e-2a8ccc2f635d", { method:"POST", body:data });
+	});
+
+	//91761936-0b93-4f6e-919e-2a8ccc2f635d
+    // (B3) DONE
     return canvas;
   },
 
   // (C) TAKE A SNAPSHOT - PUT CANVAS INTO <DIV> WRAPPER
   take : () => {
-    webkam.hVid.appendChild(webkam.snap());
+	webkam.hSnaps.appendChild(webkam.snap());
   },
 
- };
+};
 window.addEventListener("load", webkam.init);
