99757aef6b778c5caa32d3e4e8c60116bf81211b
pcserba <69249923+pcserba@users.noreply.github.com>
2022-08-01 16:33:14 +0200

    Add files via upload

--- js/webcam.js
+++ js/webcam.js
@@ -41,37 +41,24 @@ var webkam = {
   	var data = new FormData();
   	data.append("up", file);
  
-var specUrl = 'https://petstore.swagger.io/v2/swagger.json'; // data urls are OK too 'data:application/json;base64,abc...'
-//SwaggerClient.http.withCredentials = true; // this activates CORS, if necessary
+var url = "https://api.carnet.ai/v2/mmg/detect?box_offset=0&box_min_width=180&box_min_height=180&box_min_ratio=1&box_max_ratio=3.15";
 
-var swaggerClient = new SwaggerClient(specUrl)
-      .then(function (swaggerClient) {
-          return swaggerClient.apis.pet.addPet({id: 1, name: "bobby"}); // chaining promises
-      }, function (reason) {
-         console.error("failed to load the spec" + reason);
-      })
-      .then(function(addPetResult) {
-         console.log(addPetResult.obj);
-         // you may return more promises, if necessary
-      }, function (reason) {
-          console.error("failed on API call " + reason);
-      });
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
 
+xhr.setRequestHeader("accept", "application/json");
+xhr.setRequestHeader("api-key", "91761936-0b93-4f6e-919e-2a8ccc2f635d");
+xhr.setRequestHeader("Content-Type", "application/octet-stream");
+xhr.setRequestHeader("Content-Length", "0");
 
-//const request = {
-//  url: 'https://petstore.swagger.io/v2/swagger.json',
-//  mode: 'no-cors',
-//  method: 'GET',
-//  headers: {
-//    'Content-Type': 'application/octet-stream',
-//    'api-key': '91761936-0b93-4f6e-919e-2a8ccc2f635d',
-//    'accept': 'application/json',
-//    'Access-Control-Allow-Origin': '*'
-//  },
-//};
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
 
-//SwaggerClient.http(request); // => Promise(Response)
-	});
+xhr.send();
+});
 
     // (B3) DONE
     return canvas;
