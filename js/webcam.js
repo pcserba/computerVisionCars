617d5b6f5205d1be96f9ac48fe8bde7b360ff0d5
pcserba <69249923+pcserba@users.noreply.github.com>
2022-08-02 15:11:11 +0200

    Add files via upload

--- js/webcam.js
+++ js/webcam.js
@@ -46,7 +46,7 @@ const data1 = JSON.stringify({
 	"url": "https://api.carnet.ai/v2/mmg/detect?box_offset=0&box_min_width=180&box_min_height=180&box_min_ratio=1",
 	"method": "POST",
 	"params": {},
-	"data": {},
+	"data": {data},
 	"headers": {"api-key": "91761936-0b93-4f6e-919e-2a8ccc2f635d", "accept": "application/json", "Content-Type": "application/octet-stream"},
 	"cookies": {}
 });
