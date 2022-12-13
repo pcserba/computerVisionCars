d29735a94705b9bf6335a2fdf72bb6a65071a245
pcserba <69249923+pcserba@users.noreply.github.com>
2022-12-13 10:33:27 +0100

    Update cURL.php

--- php/cURL.php
+++ php/cURL.php
@@ -17,7 +17,7 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_POSTFIELDS, file_get_contents($post[file]));
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_URL, "https://api.carnet.ai/v2/mmg/detect?box_offset=0&box_min_width=100&box_min_height=100&box_min_ratio=1");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('api-key: 91761936-0b93-4f6e-919e-2a8ccc2f635d','accept: application/json','Content-Type: application/octet-//stream'));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('api-key: [request key from carnet.ai]','accept: application/json','Content-Type: application/octet-//stream'));
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
