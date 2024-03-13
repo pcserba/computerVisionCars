54f82e1537a2697fcb2930a3e9c93eadbf02b5d5
pcserba <69249923+pcserba@users.noreply.github.com>
2024-03-13 20:28:31 +0100

    Add files via upload

--- js/updateUI.js
+++ js/updateUI.js
@@ -1,11 +1,15 @@
 function showData(res) {
-document.getElementById('loading').classList.toggle('show');
+document.getElementById('loading').style.opacity=0;
 	//update the text
 	//build the string
 
+//full dataset (for backlink to EEA)
+var discomapFull = "https://discodata.eea.europa.eu/sql?query=%20%20%20%20%20%20%20%20Select%20*%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20Mh%20not%20in%20(%27AA-IVA%27%2C%27AA-NSS%27%2C%27DUPLICATE%27%2C%27UNKNOWN%27%2C%27OUT%20OF%20SCOPE%27)%0D%0A%20%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3D2020%0D%0A%09AND%20MS%20NOT%20IN%20(%27UK%27%2C%27NO%27%2C%27IS%27)%20&p=1&nrOfHits=100&mail=null&schema=null"
+
+
 if (typeof res === "undefined")
 		{
-		document.getElementById('summary').innerHTML="<P style=text-align:right; top: 0px; margin-right:0px;><a href=https://darwah-group.com/computerVision><img src=img/close-window.png width=40px></a><P style=margin-right:40px;>"+error_msg;
+		document.getElementById('summary').innerHTML="<P style=text-align:right; top: 0px; margin-right:0px;><a href=https://darwah-group.com/GoCard><img src=img/close-window.png width=40px></a><P style=margin-right:40px;>"+error_msg;
 		document.getElementById('summary').classList.toggle('show');
 		}
 		else
@@ -15,68 +19,59 @@ if (typeof res === "undefined")
 			console.log(res[0].results.length);
 			e = "true";
 			console.log(e);
-		document.getElementById('summary').innerHTML='<table><TR><TD>We detected a <C style=color:#00786C; font-weight: bold;>' + carnet["detections"][0]["mmg"][0]["make_name"] + ' </C> <C style=color:#00786C;font-weight: bold;>'+ carnet["detections"][0]["mmg"][0]["model_name"]+'</C> but could not match it in the EEA database. <TR><TD>You can browse [<a href=http://co2cars.apps.eea.europa.eu/ target=_blank>the database</A>] manually or [<a href=https://darwah-group.com/computerVision>try another vehicle</a>]<TD><a href=https://darwah-group.com/computerVision><img src=img/close-window.png width=40px></a></TD>';
+		document.getElementById('summary').innerHTML='<div style="padding: 0; width:98%; background-color:#FFFFFF; text-align:center; left: 0; right:0;"><table><TR><TD>We detected a <C style=color:#00786C; font-weight: bold;>' + carnet["detections"][0]["mmg"][0]["make_name"] + ' </C> <C style=color:#00786C;font-weight: bold;>'+ carnet["detections"][0]["mmg"][0]["model_name"]+'</C> but could not match it in the EEA database. <TD><a href=https://darwah-group.com/GoCard><img src=img/close-window.png width=40px></a></TD><TR><TD>You can browse [<a href=http://co2cars.apps.eea.europa.eu/ target=_blank>the database</A>] manually or [<a href=https://darwah-group.com/GoCard>try another vehicle</a>]<TD>';
 		document.getElementById('summary').classList.toggle('show');
 		}
 		else
 		{
-//full dataset (for backlink to EEA)
-var discomapFull = "https://discodata.eea.europa.eu/sql?query=%20%20%20%20%20%20%20%20Select%20*%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20Mh%20not%20in%20(%27AA-IVA%27%2C%27AA-NSS%27%2C%27DUPLICATE%27%2C%27UNKNOWN%27%2C%27OUT%20OF%20SCOPE%27)%0D%0A%20%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3D2020%0D%0A%09AND%20MS%20NOT%20IN%20(%27UK%27%2C%27NO%27%2C%27IS%27)%20&p=1&nrOfHits=100&mail=null&schema=null"
 
-if (res[0].results[0].avgEWLTP-114.7>0) {
-	vsAvgText = '<B style="color: #AE3446;">('+Math.round((res[0].results[0].avgEWLTP-114.7)/114.7*1000)/10+'% above EU average)';
-	}
-	else 
+
+txt = '<div style="padding: 0; width:98%; background-color:#FFFFFF; text-align:center; left: 0; right:0;"><table class=table1><TR><TD style="width:30px; vertical-align: middle;";"><a href=https://darwah-group.com/GoCard><img src=img/close-window.png width=40px></a></TD><TD style="vertical-align: middle;">'+carnet["detections"][0]["mmg"][0]["make_name"]+'</table></div><div style="padding: 0; width:98%; text-align:center; left: 0; right:0;"><table class="table2"><TR><TD style="width:33%;">'
+
+//badges
+if (res[0].results[0].avgM>2000)
 	{
-	vsAvgText = '<B style="color: #5BAA9D;">('+Math.round((res[0].results[0].avgEWLTP-114.7)/114.7*1000)/10+'% below EU average)';
+	txt = txt + '<img src=img/badge-high-weight.png width=100% height=100%>';
 	}
 
-if (res[0].results[0].avgEWLTP == 0)
+if (res[0].results[0].avgM<1000)
 	{
-	vsAvgText = '<B style="color: #5BAA9D;">(100% below EU average)';
+	txt = txt + '<img src=img/badge-low-weight.png width=100% height=100%>';
 	}
 
 
-txt = '<table><TR><TD>We detected a <C style=color:#00786C; font-weight: bold;>' + carnet["detections"][0]["mmg"][0]["make_name"] + '</C> <C style=color:#00786C; font-weight: bold;>'+res[0].results[0].Cn_short+'</C> with <B>'+Math.round(carnet["detections"][0]["mmg"][0]["probability"]*100)+'%</B> confidence.</TD><TD><a href=https://darwah-group.com/computerVision><img src=img/close-window.png width=40px></a></TD><TR><TD><B>Registrations ('+res[0].results[0].year+'):</B><TD><C style=color:#00786C;font-weight: bold;> '+(res[0].results[0].countVIN).toLocaleString()+'</C> vehicles</TD><TR><TD><B>Average CO<sub>2</sub> Emissions</B> (Ewltp): </TD><TD><C style=color:#00786C;font-weight: bold;>'+res[0].results[0].avgEWLTP+' </C> g/km</TD></table><table><TR><TD colspan=2>' + vsAvgText + '</TD><TR><TD><B>Average engine capacity:</B></TD><TD> <C style=color:#00786C;font-weight: bold;>'+(res[0].results[0].avgEc).toLocaleString()+'</C> cm<sup>3</sup></TD><TR><TD><B>Average engine power:</B></TD><TD><C style=color:#00786C; font-weight: bold;>'+(res[0].results[0].avgEp).toLocaleString()+'</C> kwh</TD><TR><TD><B>Average mass:</B></TD><TD><C style=color:#00786C; font-weight: bold;>'+(res[0].results[0].avgM).toLocaleString()+'</C> kg </TD></table><table><TR><TD><TD id=t1><a href=#>[Registrations of '+ carnet["detections"][0]["mmg"][0]["make_name"] +' '+res[0].results[0].Cn_short+' by year]</A></TD><TR><TD><TR><TD><TD id=t2><a href=#>[Registrations of '+ carnet["detections"][0]["mmg"][0]["make_name"] +' '+res[0].results[0].Cn_short+' by country in 2020]</A></TD><TR><TD><TD>Source: <a href=http://co2cars.apps.eea.europa.eu/ target=_blank>[EEA]</A> || <a href='+discomapFull+' target=_blank>[dataset]</A></table>';
+txt = txt + '</TD><TD style="width:33%;">';
+
+if (res[0].results[0].countVIN<1000)
+	{
+	txt = txt + '<img src=img/badge-rare.png width=100% height=100%>';
+	}
+
+
+txt = txt + '</TD><TD style="width:33%;">';
+
+//display badges depending on extreme values
+if (res[0].results[0].avgEWLTP>150)
+	{
+	txt = txt + '<img src=img/badge-high-emitter.png width=100% height=100%>';
+	}
+
+if (res[0].results[0].avgEWLTP<95)
+	{
+	txt = txt +'<img src=img/badge-low-emitter.png width=100% height=100%>';
+	}
+
+txt = txt + '</table></div><div style="position:absolute; bottom: 10%;left: 0; right:0; width:98%; background-color:#FFFFFF; text-align:center;"><div style="font-size:x-large; font-weight:bold;">'+res[0].results[0].Cn_short+'</div><table><TR><TD><B>Recognition confidence (%):</B><TD>'+Math.round(carnet["detections"][0]["mmg"][0]["probability"]*100)+'</C></TD><TR><TD><B>Registrations EU ('+res[0].results[0].year+'):</B><TD>'+(res[0].results[0].countVIN).toLocaleString()+'</C></TD><TR><TD><B>gCO<sub>2</sub>/km</B></TD><TD>'+res[0].results[0].avgEWLTP+' </C></TD><TR><TD><B>ccm</B></TD><TD>'+(res[0].results[0].avgEc).toLocaleString()+'</C></TD><TR><TD><B>kW</B></TD><TD>'+(res[0].results[0].avgEp).toLocaleString()+'<TR><TD><B>kg</B></TD><TD>'+(res[0].results[0].avgM).toLocaleString()+'</TD><TR><TD>Data from: <a href=http://co2cars.apps.eea.europa.eu/ target=_blank>[European Environment Agency]</A><TD></table></div>';
 
 document.getElementById('summary').innerHTML=txt;
 
-document.getElementById('t1').addEventListener('click', function(){
-   showYear(res[2].results);  });
-
-document.getElementById('t2').addEventListener('click', function(){
-   showMS(res[1].results);  });
 
 	//transition opacity
 	document.getElementById('summary').classList.toggle('show');
+	document.getElementById('canvas').style.height = c.height;
 	}
 }
 
 	};
 
 
-function showMS(res) {
-	chartByCountry(res);
-	//transition opacity
-	document.getElementById('BL').classList.add('show');
-
-}
-
-function showYear(res) {
-	chartByYear(res);
-	//transition opacity
-	document.getElementById('BL').classList.add('show');
-
-}
-
-function showManufacturer(res) {
-	chartByManufacturer(res);
-	//transition opacity
-	document.getElementById('BL').classList.add('show');
-
-}
-
-function showAll() {
-	showData();
-	showYear();	
-}
--- js/webcam.js
+++ js/webcam.js
@@ -1,18 +1,18 @@
 function convertBlob() {
 
-document.getElementById('loading').classList.toggle('show');
+document.getElementById('loading').style.opacity=1;
 
 canvas.toBlob((blob) => {
   var data = new FormData();
   data.append("up", f);
 
-fetch('../computerVision/php/cURL.php', {method: 'POST', body: data})
+fetch('../GoCard/php/cURL.php', {method: 'POST', body: data})
 
        .then(res => res.text())
 	.then(res =>
 		{
 		carnet = JSON.parse(res);
-		if (typeof carnet["detections"] === "undefined" || carnet["detections"].length==0)
+		if (typeof carnet["detections"] === "undefined" || carnet["detections"].length==0 || carnet["detections"][0]["mmg"][0]["make_name"]=="undefined")
 			{
 			e = "true";
 			error_msg="Cannot detect vehicle. Please try again.";
@@ -24,17 +24,23 @@ fetch('../computerVision/php/cURL.php', {method: 'POST', body: data})
 		else
 		{
 
+
 //remap some manufacturers and models
 switch(carnet["detections"][0]["mmg"][0]["make_name"]) {
 	case "MINI":
 		carnet["detections"][0]["mmg"][0]["make_name"]="BMW";
 		carnet["detections"][0]["mmg"][0]["model_name"]="MINI";
 	break;
-	case "Jeep":
-		carnet["detections"][0]["mmg"][0]["make_name"]="CHRYSLER";
+	case "JEEP":
+		carnet["detections"][0]["mmg"][0]["make_name"]="CHRYSLRER";
 	break;
 	case "smart":
 		carnet["detections"][0]["mmg"][0]["make_name"]="MERCEDES-BENZ AG";
+	break;
+	case "Rolls-Royce":
+		carnet["detections"][0]["mmg"][0]["make_name"]="ROLLS ROYCE";
+	case "Vauxhall":
+		carnet["detections"][0]["mmg"][0]["make_name"]="OPEL";
 	break;
 	}
 
@@ -50,25 +56,32 @@ switch(carnet["detections"][0]["mmg"][0]["model_name"]) {
 	break;
 
 }
+//for better results, we need to remap the Manufacturer to the Mh used in EEA database
+//Stellantis excluding DS automobiles
 
-var discomap1="https://discodata.eea.europa.eu/sql?query=SELECT%20%0D%0A%20%20%20%20Cn_short%0D%0A%20%20%20%20%2Cyear%0D%0A%20%20%20%20%2CISNULL(round(avg(EWLTP)%2C0),0)%20as%20avgEWLTP%0D%0A%20%20%20%20%2CISNULL(round(avg(Zr)%2C0),0)%20as%20avgZr%0D%0A%20%20%20%20%2CISNULL(round(avg(Ec)%2C0),'0')%20as%20avgEc%0D%0A%20%20%20%20%2Cround(avg(Ep)%2C0)%20as%20avgEp%0D%0A%20%20%20%20%2Cround(avg(M)%2C0)%20as%20avgM%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%0D%0A%20%20%20%20%20%20%20%20(%0D%0A%20%20%20%20%20%20%20%20Select%20Mh%0D%0A%20%20%20%20%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0D%0A%20%20%20%20%20%20%20%20%2Cyear%0D%0A%20%20%20%20%20%20%20%20%2CID%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bec%20(cm3)%5D%20as%20numeric(10%2C0))%20as%20Ec%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bep%20(KW)%5D%20as%20numeric(10%2C0))%20as%20Ep%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BEwltp%20(g%2Fkm)%5D%20as%20numeric(10%2C0))%20as%20EWLTP%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BZr%5D%20as%20numeric(10%2C0))%20as%20Zr%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BM%20(kg)%5D%20as%20numeric(10%2C0))%20as%20M%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20Mh%20LIKE%20%27%25%27%20%2B%20%27"+carnet["detections"][0]["mmg"][0]["make_name"]+"%27%20%2B%27%25%27%0D%0A%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3D2020%0D%0A%09%20%0D%0A%20%20%20%20%20%20%20%20)%20as%20a%0D%0AGROUP%20BY%20CN_short%2Cyear&p=1&nrOfHits=100&mail=null&schema=null"
+if (carnet["detections"][0]["mmg"][0]["make_name"]=='Peugeot' || carnet["detections"][0]["mmg"][0]["make_name"]=='Citroen' || carnet["detections"][0]["mmg"][0]["make_name"]=='Opel' || carnet["detections"][0]["mmg"][0]["make_name"]=='Lancia')
+{
+var discomap1="https://discodata.eea.europa.eu/sql?query=SELECT%20%0D%0A%20%20%20%20Cn_short%0D%0A%20%20%20%20%2Cyear%0D%0A%20%20%20%20%2CISNULL(round(avg(EWLTP)%2C0)%2C0)%20as%20avgEWLTP%0D%0A%20%20%20%20%2CISNULL(round(avg(Ec)%2C0)%2C0)%20as%20avgEc%0D%0A%20%20%20%20%2Cround(avg(Ep)%2C0)%20as%20avgEp%0D%0A%20%20%20%20%2Cround(avg(M)%2C0)%20as%20avgM%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%0D%0A%20%20%20%20%20%20%20%20(%0D%0A%20%20%20%20%20%20%20%20Select%20Mh%0D%0A%20%20%20%20%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0D%0A%20%20%20%20%20%20%20%20%2Cyear%0D%0A%20%20%20%20%20%20%20%20%2CID%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bec%20(cm3)%5D%20as%20integer)%20as%20Ec%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bep%20(KW)%5D%20as%20integer)%20as%20Ep%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BEwltp%20(g%2Fkm)%5D%20as%20integer)%20as%20EWLTP%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BM%20(kg)%5D%20as%20integer)%20as%20M%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20(Mh%20LIKE%20%27%25"+carnet["detections"][0]["mmg"][0]["make_name"]+"%25%27%0D%0A%20%20%20%20%20%20%20%20%20OR%20Mh%20LIKE%20%27%25STELLANTIS%25%27%0D%0A%20%20%20%20%20%20%20%20%20OR%20Mh%20LIKE%20%27%25PSA%25%27%0D%0A%20%20%20%20%20%20%20%20%20)AND%20year%3D2022%0D%0A%09%20%20%20%20)%20as%20a%0D%0AGROUP%20BY%20CN_short%2Cyear&p=1&nrOfHits=100&mail=null&schema=null";
+}
+//DS automobiles
+else if (carnet["detections"][0]["mmg"][0]["make_name"]=='DS')
+{
+var discomap1="https://discodata.eea.europa.eu/sql?query=SELECT%20%0A%20%20%20%20Cn_short%0A%20%20%20%20%2Cyear%0A%20%20%20%20%2Cround(avg(EWLTP)%2C0)%20as%20avgEWLTP%0A%20%20%20%20%2Cround(avg(Ec)%2C0)%20as%20avgEc%0A%20%20%20%20%2Cround(avg(Ep)%2C0)%20as%20avgEp%0A%20%20%20%20%2Cround(avg(M)%2C0)%20as%20avgM%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0A%20%20%20%20FROM%20%0A%20%20%20%20%20%20%20%20(%0A%20%20%20%20%20%20%20%20Select%20Mh%0A%20%20%20%20%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0A%20%20%20%20%20%20%20%20%2Cyear%0A%20%20%20%20%20%20%20%20%2CID%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bec%20(cm3)%5D%20as%20numeric(10%2C0))%20as%20Ec%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bep%20(KW)%5D%20as%20numeric(10%2C0))%20as%20Ep%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BEwltp%20(g%2Fkm)%5D%20as%20numeric(10%2C0))%20as%20EWLTP%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BM%20(kg)%5D%20as%20numeric(10%2C0))%20as%20M%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20LIKE%20%27%25%27%2BREPLACE(%27DS"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0A%20%20%20%20%20%20%20%20AND%20(Mh%20LIKE%20(%27Stellantis%25%27)%20OR%20Mh%20LIKE%20%27Automobiles%25%27%20OR%20MH%20Like%27PSA%27)%0A%20%20%20%20%20%20%20%20AND%20year%3D2022%0A%20%20%20%20%20%20%20%20)%20as%20a%0A%20%20%20%20GROUP%20BY%20Cn_short%2Cyear&p=1&nrOfHits=100&mail=null&schema=null";
+}
 
-//result by country
-var discomap2="https://discodata.eea.europa.eu/sql?query=SELECT%20%0D%0A%09MS%0D%0A%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0D%0A%20%20%20%20%2Cyear%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%0D%0A%20%20%20%20%20%20%20%20(%0D%0A%20%20%20%20%20%20%20%20Select%20%0D%0A%20%20%20%20%20%20%20%20MS%0D%0A%20%20%20%20%20%20%20%20%2Cyear%0D%0A%20%20%20%20%20%20%20%20%2CID%0D%0A%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20LIKE%20%27%25%27%2B%20REPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%20%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20Mh%20LIKE%20%27%25%27%20%2B%20%27"+carnet["detections"][0]["mmg"][0]["make_name"]+"%27%20%2B%27%25%27%0D%0A%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3D2020%0D%0A%20%20%20%20%20%20%20%20GROUP%20BY%20MS%2Cyear%2CID)%20as%20a%0D%0AGROUP%20BY%20ms%2C%20year%0D%0A&p=1&nrOfHits=100&mail=null&schema=null";
 
-//result by year
-var discomap3="https://discodata.eea.europa.eu/sql?query=SELECT%20%0D%0A%20%20%20%20%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0D%0A%20%20%20%20%2Cyear%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%0D%0A%20%20%20%20%20%20%20%20(%0D%0A%20%20%20%20%20%20%20%20Select%20%0D%0A%20%20%20%20%20%20%20%20MS%0D%0A%20%20%20%20%20%20%20%20%2Cyear%0D%0A%20%20%20%20%20%20%20%20%2CID%0D%0A%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20LIKE%20%27%25%27%20%2B%20REPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20AND%20Mh%20LIKE%20%27%25%27%20%2B%20%27"+carnet["detections"][0]["mmg"][0]["make_name"]+"%27%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3E2016%0D%0AGROUP%20BY%20MS%2Cyear%2CID)%20as%20a%0D%0AGROUP%20BY%20year%0D&p=1&nrOfHits=100&mail=null&schema=null"
-
-//result by manufacturer
-var discomap4 = "https://discodata.eea.europa.eu/sql?query=SELECT%20%20%20%0D%0A%20%20%20%20Mh%0D%0A%20%20%20%20%2Cround(avg(cast(%5BEWLTP%20(g%2Fkm)%5D%20as%20float))%2C3)%20as%20avgEWLTP%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20WHERE%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BMh%5D%20in%20(%0D%0A%20%20%20%20%20%20%20%20Select%20%0D%0A%20%20%20%20%20%20%20%20DISTINCT%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BMh%5D%20as%20Mh%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20Mh%20LIKE%20%27%25%27%20%2B%20%27"+carnet["detections"][0]["mmg"][0]["make_name"]+"%27%20%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20year%3D2020%0D%0A%20%20%20%20%20%20%20%20--AND%20status%3D%27P%27%0D%0A%20%20%20%20%20%20%20%20)%0D%0A%20%20%20%20GROUP%20BY%20mh&p=1&nrOfHits=100&mail=null&schema=null"
-console.log(discomap1)
-
+/*else if (carnet["detections"][0]["mmg"][0]["make_name"]=='BMW')
+{
+var discomap1="https://discodata.eea.europa.eu/sql?query=SELECT%20%0A%20%20%20%20Cn_short%0A%20%20%20%20%2Cyear%0A%20%20%20%20%2Cround(avg(EWLTP)%2C0)%20as%20avgEWLTP%0A%20%20%20%20%2Cround(avg(Ec)%2C0)%20as%20avgEc%0A%20%20%20%20%2Cround(avg(Ep)%2C0)%20as%20avgEp%0A%20%20%20%20%2Cround(avg(M)%2C0)%20as%20avgM%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0A%20%20%20%20FROM%20%0A%20%20%20%20%20%20%20%20(%0A%20%20%20%20%20%20%20%20Select%20Mh%0A%20%20%20%20%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0A%20%20%20%20%20%20%20%20%2Cyear%0A%20%20%20%20%20%20%20%20%2CID%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bec%20(cm3)%5D%20as%20numeric(10%2C0))%20as%20Ec%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bep%20(KW)%5D%20as%20numeric(10%2C0))%20as%20Ep%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BEwltp%20(g%2Fkm)%5D%20as%20numeric(10%2C0))%20as%20EWLTP%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BM%20(kg)%5D%20as%20numeric(10%2C0))%20as%20M%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0A%20%20%20%20%20%20%20%20WHERE%20Cn%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20series%27%2C%27%27)%2B%27%25%27%0A%20%20%20%20%20%20%20%20AND%20Mh%20LIKE%20%27%25BMW%25%27%0A%20%20%20%20%20%20%20%20AND%20year%3D2022%0A%20%20%20%20%20%20%20%20)%20as%20a%0A%20%20%20%20GROUP%20BY%20Cn_short%2Cyear&p=1&nrOfHits=100&mail=null&schema=null";
+}*/
+else
+{
+var discomap1="https://discodata.eea.europa.eu/sql?query=SELECT%20%0D%0A%20%20%20%20Cn_short%0D%0A%20%20%20%20%2Cyear%0D%0A%20%20%20%20%2CISNULL(round(avg(EWLTP)%2C0)%2C0)%20as%20avgEWLTP%0D%0A%20%20%20%20%2CISNULL(round(avg(Ec)%2C0)%2C0)%20as%20avgEc%0D%0A%20%20%20%20%2Cround(avg(Ep)%2C0)%20as%20avgEp%0D%0A%20%20%20%20%2Cround(avg(M)%2C0)%20as%20avgM%0D%0A%20%20%20%20%2Ccount(distinct%20ID)%20as%20countVIN%0D%0A%20%20%20%20FROM%20%0D%0A%20%20%20%20%20%20%20%20(%0D%0A%20%20%20%20%20%20%20%20Select%20Mh%0D%0A%20%20%20%20%20%20%20%20%2C%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%20as%20Cn_short%0D%0A%20%20%20%20%20%20%20%20%2Cyear%0D%0A%20%20%20%20%20%20%20%20%2CID%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bec%20(cm3)%5D%20as%20integer)%20as%20Ec%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5Bep%20(KW)%5D%20as%20integer)%20as%20Ep%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BEwltp%20(g%2Fkm)%5D%20as%20integer)%20as%20EWLTP%0D%0A%20%20%20%20%20%20%20%20%2Ccast(%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D.%5BM%20(kg)%5D%20as%20integer)%20as%20M%0D%0A%20%20%20%20%20%20%20%20from%20%5BCO2Emission%5D.%5Blatest%5D.%5Bco2cars%5D%0D%0A%20%20%20%20%20%20%20%20WHERE%20REPLACE(Cn%2C%27%20%27%2C%27%27)%20%20LIKE%20%27%25%27%2BREPLACE(%27"+carnet["detections"][0]["mmg"][0]["model_name"]+"%27%2C%27%20%27%2C%27%27)%2B%27%25%27%0D%0A%20%20%20%20%20%20%20%20AND%20Mh%20LIKE%20%27%25"+carnet["detections"][0]["mmg"][0]["make_name"]+"%25%27%0D%0A%20%20%20%20%20%20%20%20%20AND%20year%3D2022%0D%0A%09%20%20%20%20)%20as%20a%0D%0AGROUP%20BY%20CN_short%2Cyear&p=1&nrOfHits=100&mail=null&schema=null";
+}
+console.log(discomap1);
 const fetchReq1 = fetch(discomap1).then(res=>res.json());
-const fetchReq2 = fetch(discomap2).then(res=>res.json());
-const fetchReq3 = fetch(discomap3).then(res=>res.json());
-//const fetchReq4 = fetch(discomap4).then(res=>res.json());
 
-const allData = Promise.all([fetchReq1,fetchReq2,fetchReq3]);
+const allData = Promise.all([fetchReq1]);
 // attach then() handler to the allData Promise
 allData.then((res) => {
 	showData(res)});
